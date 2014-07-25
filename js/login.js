6f9d60e1594a758f3003763a9cd105167eb75f63
David Ugale <dugale@dayaweb.com>
2014-07-24 22:09:34 -0700

    login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,7 @@
 function logIn() {
+alert(jQuery("#email").val());
+alert(jQuery("#password").val());
+
     if (jQuery("#email").val() == 'dugale@dayaweb.com' && jQuery("#password").val() == 'testing') {
         window.localStorage.setItem("id", "1");            
         window.location.href = 'home.html';
