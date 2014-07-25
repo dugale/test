8874409cef24b85c255dc032adf1aea01f36e75c
David Ugale <dugale@dayaweb.com>
2014-07-24 22:25:18 -0700

    auth

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,11 @@
 function isLoggedIn() {
     var id = window.localStorage.getItem("id");
+alert(id);
+/*
     if (id) {
         window.location.href = 'home.html';
     } else {
         window.location.href = 'login.html';
     }
+*/
 }
