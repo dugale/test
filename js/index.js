9dc7728cdbe6f4c29cee9a85d10f230d5a2c7cab
scorzo <seancorzo@gmail.com>
2014-08-22 20:12:01 -0700

    added login

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        console.log('[initialize]');
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -26,6 +27,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
+        console.log('[bindEvents]');
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -33,85 +35,138 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        console.log('[onDeviceReady]');
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        console.log('[receivedEvent: ' +  id + ']');
 
-        /*
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-        */
-
-        console.log('Received Event: ' + id);
-
+        jQuery.mobile.changePage('#loginPage');
+        
         $("#loginForm").on("submit",this.handleLogin);
 
     },
 
-    checkPreAuth: function() {
-        var form = $("#loginForm");
-        if(window.localStorage["email"] != undefined && window.localStorage["password"] != undefined) {
-            $("#email", form).val(window.localStorage["email"]);
-            $("#password", form).val(window.localStorage["password"]);
-
-            console.log('Previous login information found.');
-            this.handleLogin();
-        }
-        else {
-            console.log('Previous login information was not found.');
+    "servers": {
+        "query": function (url, type, data, callback) { console.log("[query "+url+"]");
+            $.ajax(url, {
+                "type": type,
+                "dataType": "json",
+                "data": data,
+                "contentType": (type==="GET" ? "application/json" : "application/x-www-form-urlencoded"),
+                "success": callback,
+                "error": function (request, status, error) {
+                    console.log("[ajax error:" + request.responseText + "]");
+                }
+            });
+        },
+        "public": {
+            "URL":  "http://u-vend.dayawebdevelopment.com/app/request",
+            // perform unauthenticated query
+            "query": function (action, data, callback) { console.log("[public.query]");
+                app.servers.query(app.servers.public.URL+action, "GET", data, callback);
+            }
+        },
+        "private": {
+            "URL": "http://u-vend.dayawebdevelopment.com/app/request",
+            // perform authenticated query
+            "query": function (action, data, callback) { console.log("[private.query]");
+                if (!localStorage || !localStorage.getItem("token_public") || !localStorage.getItem("token_private")) {
+                    console.log("can't do private query: empty private/public tokens!");
+                    return;
+                }
+                // used as seed for hashing the private token
+                var timestamp = Math.round(+new Date()/1000);
+                app.servers.query(app.servers.private.URL+action, "POST", {
+                        "timestamp": timestamp,
+                        "token_public": localStorage.getItem("token_public"),
+                        //"hash": sha1(timestamp+localStorage.getItem("token_private")),
+                        "hash": sha1(localStorage.getItem("token_private")),
+                        "data": data
+                }, callback);
+            }
         }
     },
-
     handleLogin: function() {
+
+        console.log('[handlLogin]');
+
         var form = $("#loginForm");    
         //disable the button so we can't resubmit while we wait
         $("#submitButton",form).attr("disabled","disabled");
         var e = $("#email", form).val();
         var p = $("#password", form).val();
-        console.log("[handleLogin]");
-
-
 
         if(e != '' && p!= '') {
-            console.log("sending email and password");
+            console.log("[Sending email and password.]");
 
-            $.ajax({
-                url: "http://u-vend.dayawebdevelopment.com/app/login",
-                type: "POST",
-                data: {email:e,password:p}, 
-                success: function(res) {
-                    if(res.result == true) {
-                        console.log("login succeeded");
-                        //store
-                        window.localStorage["email"] = e;
-                        window.localStorage["password"] = p;             
-                        $.mobile.changePage("home.html");
-                    } else {
-                        console.log("got false");
-                        navigator.notification.alert("Your login failed", function() {});
-                    }
-                    $("#submitButton").removeAttr("disabled");
-                },
-                error: function (jqXHR, textStatus, errorThrown) { 
-                    console.log("Connection With Server Failed", jqXHR.error + ' ' + textStatus + ' ' + errorThrown);
-                },
-                dataType: "json"
-            });
+            app.servers.public.query("login", {
+                "email":e,
+                "password":p
+            }, app.callbacks.handleLogin, true);   
+        }
+        else {
+            console.log("[Login failed.]", function() {});
+            $("#submitButton").removeAttr("disabled");
         }
         return false;
-    }
+    },
+    "callbacks": {
+        "handleLogin": function(r) {
+            console.log("[callback: handleLogin");
+            console.log("[" + r.token_public + "]");
+            if(r && r.code && r.code === "SUCCESS" && r.token_private && r.token_public) {
+                console.log("[Login succeeded.]");
+                //store
+                localStorage["token_private"] = r.token_private;
+                localStorage["token_public"] = r.token_public;
+                // redirect brands
+                console.log("[Redirecting to brands.]");
+                $("#submitButton").removeAttr("disabled");
+                jQuery.mobile.changePage('#brandsPage');
+            } else {
+                console.log("[Login failed.]", function() {});
+                $("#submitButton").removeAttr("disabled");
+            }
+        },
+        "fetchBrands": function(r) { 
 
+            console.log("[fetchBrands callback]");
 
+            if(r && r.code && r.code === "SUCCESS") {
+                console.log("[fetchBrands SUCCESS]");
+                $("#brandsDisplay").html( r.brands );
+            }
+            else {
+                console.log("[fetchBrands ERROR]");
+                console.log("[fetchBrands message:" + r.message + "]");
+                console.log("[Redirecting to login.]");
+                jQuery.mobile.changePage('#loginPage');
+            }
 
+        },
+    },
+    fetchBrands: function() {
+        
+        console.log["fetchBrands"];
 
+        app.servers.private.query('fetchbrands', {}, app.callbacks.fetchBrands);
+    },
+    logout: function() {
+        
+        console.log["logout"];
 
+        if (localStorage) {
+            //console.log("[Public key before clearning localStorage:" + localStorage.getItem("token_public") + "]");
+            localStorage.clear();
+            //console.log("[Public key after clearning localStorage:" + localStorage.getItem("token_public") + "]");
+        }
 
+        console.log("[Redirecting to login.]");
+        jQuery.mobile.changePage('#loginPage');
 
+    },
 
 };
 
