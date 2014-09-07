4325b4f30cdf04908ca800b01daed938fe40d535
scorzo <seancorzo@gmail.com>
2014-09-06 23:09:26 -0700

    added some server side link and form checking - added slide transitions

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -38,13 +39,24 @@ var app = {
         console.log('[onDeviceReady]');
         app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
+
+    /*
+    // bind events
+    */
+
     receivedEvent: function(id) {
         console.log('[receivedEvent: ' +  id + ']');
 
         jQuery.mobile.changePage('#loginPage');
 
         $("#loginForm").on("submit",this.handleLogin);
+
+        // disable for now
+        // $("#flavorAddForm").on("submit",this.machineFlavorAddSubmit);
+        $("#flavorAddSubmitButton").attr("disabled","disabled");
+        $("#flavorEditUpdateSubmitButton").attr("disabled","disabled");
+        $("#flavorEditDeleteSubmitButton").attr("disabled","disabled");
+        $("#rollsOfCoinEditSubmitButton").attr("disabled","disabled");
 
         // bind the "on click" event listner only once during initial page else will get added multiple times when "back" button is used
         $(document).on("pagecreate", "#machineCupsPage", app.machineCupTotalsAttachClickListner);
@@ -56,7 +68,16 @@ var app = {
         // populate list view every time page is visited 
         $(document).on("pagebeforeshow", "#machineFlavorsPage", app.fetchMachineFlavors);
 
+        // populate form every time page is visited
+        $(document).on("pagebeforeshow", "#machineFlavorEditPage", app.machineFlavorEdit);
+
+        // populate list view every time page is visited 
+        $(document).on("pagebeforeshow", "#machineFlavorAddPage", app.machineFlavorAdd);
     },
+
+    /*
+    // json helpers
+    */
 
     "servers": {
         "query": function (url, type, data, callback) { console.log("[query "+url+"]");
@@ -98,6 +119,11 @@ var app = {
             }
         }
     },
+
+    /*
+    // login stuff
+    */
+
     handleLogin: function() {
 
         console.log('[handlLogin]');
@@ -136,11 +162,17 @@ var app = {
         jQuery.mobile.changePage('#loginPage');
 
     },
+
+    /*
+    // bound to events
+    */
+
     fetchMachineCupTotals: function() {
         
         console.log["fetchMachineCupTotals"];
 
         app.servers.private.query('fetchmachinecuptotals', {}, app.callbacks.fetchMachineCupTotals);
+
     },
     machineCupTotalsAttachClickListner: function() {
 
@@ -150,20 +182,20 @@ var app = {
         $("#machineCupsPageList").on("click", "li", function (e) {
             // override default event action
             e.preventDefault();
-            //store the information in the next page's data
-            $("#machineFlavorsPage").data("info", { id: this.id });
+
+            localStorage["machineId"] = this.id;
+
             // redirect
             console.log("[machineCupsPageList forwarding to machineFlavorsPage]");
-            $.mobile.changePage("#machineFlavorsPage");
+            $.mobile.changePage("#machineFlavorsPage", {transition: "slidefade"});
         });
+
     },
     fetchMachineFlavors: function() {
         
         console.log["fetchMachineFlavors"];
 
-        //populated when the list item was clicked in previous page
-        var info = $('#machineFlavorsPage').data("info");
-        var mId = info['id'];
+        var mId = localStorage.getItem("machineId");
 
         app.servers.private.query('fetchmachineflavors', {machineId:mId}, app.callbacks.fetchMachineFlavors);
     },
@@ -173,12 +205,65 @@ var app = {
 
         $("#machineFlavorsPageList").on("click", "li", function (e) {
             e.preventDefault();
-            //store the information in the next page's data
-            $("#machineFlavorEditPage").data("info", { id: this.id });
+
+            localStorage["machineFlavorId"] = this.id;
+
             // redirect
-            $.mobile.changePage("#machineFlavorEditPage");
+            $.mobile.changePage("#machineFlavorEditPage", {transition: "slidefade"});
         });
     },
+
+    machineFlavorEdit: function() {
+        
+        console.log["machineFlavorEdit"];
+
+        var mfId = localStorage.getItem("machineFlavorId");
+
+        app.servers.private.query('fetchmachineflavor', {machineFlavorId:mfId}, app.callbacks.machineFlavorEdit);
+    },
+
+
+    machineFlavorAdd: function() {
+        
+        console.log["machineFlavorAdd"];
+
+        var mId = localStorage.getItem("machineId");
+
+        // fetch machine name (type and id)
+        // fetch flavor options
+        app.servers.private.query('fetchmachineavailableflavors', {machineId:mId}, app.callbacks.machineFlavorAdd);
+
+    },
+
+    machineFlavorAddSubmit: function() {
+
+        console.log('[machineFlavorAddSubmit]');
+
+        var form = $("#flavorAddForm");    
+        //disable the button so we can't resubmit while we wait
+        $("#flavorAddSubmitButton",form).attr("disabled","disabled");
+
+        var mId = localStorage.getItem("machineId");
+        var fId = $("#flavorAddFlavor", form).val();
+        var cups = $("#flavorAddCupsCount", form).val();
+
+        // add form validation 
+        if(1) {
+            console.log("[Sending add flavor info.]");
+
+            app.servers.private.query('addmachineflavor', {machineId:mId,flavorId:fId,}, app.callbacks.machineFlavorAdd);
+        }
+        else {
+            console.log("[Login failed.]", function() {});
+            $("#submitButton").removeAttr("disabled");
+        }
+        return false;
+    },
+
+    /*
+    //  json callbacks 
+    */
+
     "callbacks": {
         "handleLogin": function(r) {
             console.log("[handleLogin callback");
@@ -222,6 +307,8 @@ var app = {
                     // refresh listview so that jq mobile applies styles to added li elements
                     $(this).listview("refresh");
                 });
+
+
             }
             else {
                 console.log("[fetchMachineCupTotals ERROR]");
@@ -235,8 +322,22 @@ var app = {
 
             console.log("[fetchMachineFlavors callback]");
 
+            // auth success
             if(r && r.code && r.code === "SUCCESS") {
                 console.log("[fetchMachineFlavors SUCCESS]");
+
+                // data error - customize modal dialog and return
+                if (r.result === false) {
+                    console.log("[fetchMachineFlavors: " + r.message + "]");
+                    console.log("[fetchMachineFlavors redirecting to #machineCupsPage]");
+
+                    $('#modalDialogMessage').html(r.message);
+                    $('#modalDialogRedirect').attr('href','#machineCupsPage');
+
+                    jQuery.mobile.changePage('#modalDialogTest');
+
+                    return;
+                }
 
                 var output = '';
                 $.each(r.machineFlavors, function(index, value){                   
@@ -246,12 +347,6 @@ var app = {
                     var fId = value.flavorId;
                     var fName = value.flavorName;
                     var fQuantity = value.flavorQuantity;
-
-                    console.log("[fetchMachineFlavors mId:" + mId + "]");
-                    console.log("[fetchMachineFlavors mfId:" + mfId + "]");
-                    console.log("[fetchMachineFlavors fName:" + fName + "]");
-                    console.log("[fetchMachineFlavors fQuantity:" + fQuantity + "]");
-                    console.log("[fetchMachineFlavors ------------------]");
 
                     output += '<li id="'+ mfId +'"><a href="#machineFlavorEditPage">' + fName + '<span class="ui-li-count">' + fQuantity + '</span></a></li>';
                 });
@@ -273,7 +368,87 @@ var app = {
             }
 
         },
-    }, // end callbacks
+        "machineFlavorEdit": function(r) { 
+
+            console.log("[machineFlavorEdit callback]");
+
+            if(r && r.code && r.code === "SUCCESS") {
+                console.log("[machineFlavorEdit SUCCESS]");
+
+                mf = r.machineFlavor;
+
+                var machineId = mf.machineId;
+                var machineType = mf.machineType;
+                var machineFlavorId = mf.machineFlavorId;
+                var flavorId = mf.flavorId;
+                var flavorName = mf.flavorName;
+                var flavorQuantity = mf.flavorQuantity;
+
+                $("#flavoredit-machine").val(machineType + '-' + machineId);
+                $("#flavoredit-flavor").val(flavorName);
+                $("#flavoredit-count").val(flavorQuantity);
+
+            }
+            else {
+                console.log("[machineFlavorEdit ERROR]");
+                console.log("[machineFlavorEdit message:" + r.message + "]");
+                console.log("[Redirecting to login.]");
+                jQuery.mobile.changePage('#loginPage');
+            }
+
+        },
+        "machineFlavorAdd": function(r) { 
+
+            console.log("[machineFlavorAdd callback]");
+
+            if(r && r.code && r.code === "SUCCESS") {
+                console.log("[machineFlavorAdd SUCCESS]");
+
+                // data error - customize modal dialog and return
+                if (r.result === false) {
+                    console.log("[fetchMachineFlavors: " + r.message + "]");
+                    console.log("[fetchMachineFlavors redirecting to #machineCupsPage]");
+
+                    $('#modalDialogMessage').html(r.message);
+                    $('#modalDialogRedirect').attr('href','#machineCupsPage');
+
+                    jQuery.mobile.changePage('#modalDialogTest');
+
+                    return;
+                }
+
+                all = r.machineAvailableFlavors;
+
+                var machineId = all.machineId;
+                var machineType = all.machineType;
+                var brandId = all.brandId;
+                var flavors = all.flavors;
+
+                $("#flavorAddMachine").val(machineType + '-' + machineId);
+
+                var output = '';
+                $.each(flavors, function(index, value){                   
+
+                    var fId = value.flavorId;
+                    var fName = value.flavorName;
+
+                    output += '<option value="'+ fId +'">' + fName + '</option>';
+                });
+
+                //update dropdown
+                $("#flavorAddFlavor").html(output)
+
+
+            }
+            else {
+                console.log("[machineFlavorEdit ERROR]");
+                console.log("[machineFlavorEdit message:" + r.message + "]");
+                console.log("[Redirecting to login.]");
+                jQuery.mobile.changePage('#loginPage');
+            }
+
+        },
+    }, 
 
 
 };
