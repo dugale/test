db5e3ae438640177b5d7d2d80ea11cc167518ed1
scorzo <seancorzo@gmail.com>
2014-10-13 16:22:02 -0700

    added user-scalable no to viewport meta data in order to fix annoying transition flicker

--- js/index.js
+++ js/index.js
@@ -350,20 +350,16 @@ var app = {
                 "beforeSend": function(){
                     // display spinner during ajax call
                     // have to wrap it in setInterval for it to work
-                    /*
                     var interval = setInterval(function(){
                         $.mobile.loading('show');
                         clearInterval(interval);
                     },1);   
-                    */
                 },
                 "complete": function(){
-                    /*
                     var interval = setInterval(function(){
                         $.mobile.loading('hide');
                         clearInterval(interval);
                     },1);   
-                    */
                 },
                 "error": function (request, status, error) {
                     console.log("[ajax error:" + request.responseText + "]");
